2355a8c09278dc07ebaeae21d9458e9c5a6c2d17
Pedro <pedroayz@gmail.com>
2020-10-23 00:57:56 -0700

    + Cards Modals + tooltips

--- js/navbarJs.js
+++ js/navbarJs.js
@@ -20,7 +20,35 @@ $(window).on("scroll", function() {
     else {
           $('.fixed-top').removeClass('nav-scrolled');
           $('.fixed-top').addClass('bg-transparent');
-          $('a').removeClass('.btn btn-light btn-lg ');
+          $('a').removeClass('.btn btn-light btn-lg btn-sm ');
        
     }
 })
+
+
+/* ACORDEON */
+/* $(function() {
+      var Accordion = function(el, multiple) {
+      this.el = el || {};
+      this.multiple = multiple || false;
+      
+      var links = this.el.find('.link');
+      
+      links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+      }
+      
+      Accordion.prototype.dropdown = function(e) {
+      var $el = e.data.el;
+      $this = $(this),
+      $next = $this.next();
+      
+      $next.slideToggle();
+      $this.parent().toggleClass('open');
+      
+      if (!e.data.multiple) {
+      $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+      };
+      }
+      
+      var accordion = new Accordion($('#accordion'), false);
+      }); */
