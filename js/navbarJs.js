6938a21aba63e17fe02baeb45d09f09fffc9f369
Pedro <pedroayz@gmail.com>
2020-10-25 22:52:01 -0700

    Automatizaciones agregadas:scss to css + img+html+css+js minifi + dist version // npm run peter

--- js/navbarJs.js
+++ js/navbarJs.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
     $(".menu-icon").on("click", function() {
-          $("nav ul").toggleClass("showing");
+    $("nav ul").toggleClass("showing");
     });
 });
 
-// Scrolling Effect
+                                    /*  Scrolling Effect */
+                                    /*  Scrolling Effect */
 
 $(window).on("scroll", function() {
     if($(window).scrollTop()) {
@@ -19,48 +20,73 @@ $(window).on("scroll", function() {
           $('.fixed-top').removeClass('nav-scrolled');
           $('.fixed-top').addClass('bg-transparent');
           $('a').removeClass('.btn btn-light btn-lg btn-sm ');     
-    }
+       }
 });
 
+
+                                    /* CARRUSEL */
+                                    /* CARRUSEL */
   $(function(){
     $("[data-toggle='tooltip']").tooltip();
     $("[data-toggle='popover']").popover();
     
     $('#carouselExampleFade').carousel({
       interval:600
-    });
-    
-       /* MODAL COUTER */
-  
+    });       
+                              /* MODAL COUNTER */
+                              /* MODAL COUNTER */
 
       $(window).on('show.bs.modal', function (e) {
         console.log('el modal se esta mostrando');
 
         $("#reservaModal").removeClass('btn-reserva');
-        $("#reservaModal").addClass('btn-warning');
+        $("#reservaModal").addClass('btn-danger btn-lg btn-block');
+
         $("#reservaModal").prop('disabled',true); 
       });
 
       $(window).on('hide.bs.modal', function (e) {
             console.log('el modal se esta esconcdiendo');
-
+            $("#reservaModal").removeClass('btn-danger btn-lg btn-block');
+       $("#reservaModal").addClass('btn-reserva btn-lg');
       });
 
       $(window).on('shown.bs.modal', function (e) {
             console.log('el modal se esta mostro');
-      });
-
-     
+      });   
 
       $(window).on('hidden.bs.modal', function (e) {
             console.log('el modal se escondio');
 
+      });
+
+                              /* IMAGEMIN */
+                              /* IMAGEMIN */
+
+            const imagemin = require('imagemin');
+      const imageminJpegtran = require('imagemin-jpegtran');
+      const imageminPngquant = require('imagemin-pngquant');
+            
+      (async () => {
+            const files = await imagemin(['images/*.{jpg,png}'], {
+                  destination: 'build/images',
+                  plugins: [
+                  imageminJpegtran(),
+                  imageminPngquant({
+                        quality: [0.5, 0.5]
+                  })
+                  ]
             });
+            
+            console.log(files);
+            //=> [{data: <Buffer 89 50 4e …>, destinationPath: 'build/images/foo.jpg'}, …]
+      })();
 
-      
-         
 
 
 
-  });
+/* CLOSE TAG */
+  })
 
+
+            
